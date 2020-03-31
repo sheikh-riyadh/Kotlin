f24eab207f7c31e066d6ea80d9dcd2537c772c9c
Sheikh Riyadh <nsprtime@gmail.com>
2020-03-31 01:21:14 -0700

    We write easy program how to use loop in kotlin

---
+++ src/kotlin_practice/LoopDemo.kt
@@ -0,0 +1,11 @@
+package kotlin_practice
+
+fun main(args: Array<String>) {
+    for (i in 1..10){
+        if (i%2==0){
+            println("Number is even $i")
+        }else{
+            println("Number is odd $i")
+        }
+    }
+}
