65948f965bb8ffd7a0cfff42ca277e07eb5810dc
Sheikh Riyadh <nsprtime@gmail.com>
2020-03-31 10:08:31 -0700

    Function demo

---
+++ src/kotlin_practice/FunctionDemo.kt
@@ -0,0 +1,13 @@
+package kotlin_practice
+
+fun main(args:Array<String>) {
+
+    isDisplay("Riyadh",350883 ,"Computer")
+}
+
+fun isDisplay(name:String, roll:Int ,department:String) {
+
+    println("Name : $name")
+    println("Roll : $roll")
+    println("Department : $department")
+}
