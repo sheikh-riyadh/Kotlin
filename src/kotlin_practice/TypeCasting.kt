a268b38d276fda3b3fec37ff822ca1d374f21a61
Sheikh Riyadh <nsprtime@gmail.com>
2020-03-31 01:31:34 -0700

    In this project we will show type casting

---
+++ src/kotlin_practice/TypeCasting.kt
@@ -0,0 +1,18 @@
+package kotlin_practice
+
+fun main(args:Array<String>) {
+    // Write a program a data type convert to other's data type
+    var number1 : Int =10
+    var number2 : Double = number1.toDouble()
+
+    println("Number1 = $number1")
+    println("Number = $number2")
+
+    var name : String ="4210"
+    var number3 : Int = name.toInt()
+
+    println("Name $name")
+    println("number3 $number3")
+
+
+}
