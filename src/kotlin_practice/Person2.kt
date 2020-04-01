233d52666b5cb56c15697fb55516f5bb9074f8a8
Sheikh Riyadh <nsprtime@gmail.com>
2020-04-01 01:50:36 -0700

    Person class

---
+++ src/kotlin_practice/Person2.kt
@@ -0,0 +1,10 @@
+package kotlin_practice
+
+open class Person2 (var name:String, var roll: Int, var department:String) {
+
+    fun isDisplay(){
+        println("Name :$name")
+        println("Roll :$roll")
+        println("Department :$department")
+    }
+}
---
+++ src/kotlin_practice/PersonDemo.kt
@@ -0,0 +1,8 @@
+package kotlin_practice
+
+fun main(args:Array<String>) {
+
+    var personObject = Person2("Sheikh Riyadh",350883,"Computer")
+    personObject.isDisplay()
+
+}
