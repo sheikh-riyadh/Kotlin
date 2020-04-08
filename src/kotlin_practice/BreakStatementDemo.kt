d44c158803028546e7b4337f4d0192a36d42fcf3
Sheikh Riyadh <nsprtime@gmail.com>
2020-04-07 23:46:57 -0700

    We just commented

--- src/kotlin_practice/BreakStatementDemo.kt
+++ src/kotlin_practice/BreakStatementDemo.kt
@@ -4,8 +4,8 @@ fun main(args:Array<String>) {
 
     // this break work actually inner loop
 
-    for (k in 1..3){
-        for (l in 1..3){
+    for (k in 1..3){   // outer loop
+        for (l in 1..3){  // inner loop
             println("$k"+"$l")
             if (k==1 && l==1){
               break
@@ -17,8 +17,8 @@ fun main(args:Array<String>) {
 
     // And this break work inner and outer loop
 
-   myloop@ for (i in 1..3){
-      for (j in 1..3){
+   myloop@ for (i in 1..3){ // inner loop
+      for (j in 1..3){ // outer loop
           println("$i"+"$j")
           if (i==2 && j==2){
               break@myloop
