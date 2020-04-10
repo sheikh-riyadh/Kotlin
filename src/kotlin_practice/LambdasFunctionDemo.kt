fd568f7ad2270ac90f840cab41ddf549c3ba5dbc
Sheikh Riyadh <nsprtime@gmail.com>
2020-04-10 03:56:27 -0700

    Modifier our lambdas function we used high level function

--- src/kotlin_practice/LambdasFunctionDemo.kt
+++ src/kotlin_practice/LambdasFunctionDemo.kt
@@ -5,8 +5,19 @@ fun main(args:Array<String>) {
      var result = ob(5,7)
     println(result)
 
+    higherFunction(7, 1 ,{x,y -> x + y})
+
 }
 
 var ob :(Int, Int) -> Int = {x,y  ->  x+y} // this the lambdas function
+
+
+fun higherFunction(a: Int , b: Int , lambdas : (Int, Int)->Int){
+    var valueOf = a * b
+    println(valueOf)
+
+   var result = lambdas(a,b)
+    println(result)
+}
 
 
