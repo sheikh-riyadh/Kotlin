d7813c76acd3e2718b38e0cd7fbe8cda959ce5b9
Sheikh Riyadh <nsprtime@gmail.com>
2020-04-12 21:53:42 -0700

    Map demo

--- src/kotlin_practice/MapDemo.kt
+++ src/kotlin_practice/MapDemo.kt
@@ -2,10 +2,9 @@ package kotlin_practice
 
 fun main(args:Array<String>) {
 
-    var list = listOf<Int>(5,7,8,2,4,6,23,9,10)
-
-    var squareNumber = list.map { it*it }
-
+    var list = listOf<Int>(5,7,8,2,4,6)
+                                            // it actually 5,7,8,2,4,6
+    var squareNumber = list.map { it*it } // it*it means (5*5,7*7,8*8,2*2,4*4,6*6)
     println("Number of square = $squareNumber \n") // this line also print together
 
 
